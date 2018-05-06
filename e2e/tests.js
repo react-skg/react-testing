8d3a350afcc1f89d8b2fdb25f9ba01676beb3d26
Pavlos <pkoutoglou@gmail.com>
2018-05-06 21:57:10 +0300

    Rename 2e2Tests to e2e
    Rename 2e2Tests to e2e

---
+++ e2e/tests.js
@@ -0,0 +1,20 @@
+﻿var webdriverio = require("webdriverio");
+var options = {
+  desiredCapabilities: {
+    browserName: "chrome"
+  }
+};
+
+webdriverio
+  .remote(options)
+  .init()
+  .url("https://testwebapp20180417085330.azurewebsites.net/")
+  .getTitle()
+  .then(function(title) {
+    console.log("Title was: " + title);
+  })
+  .pause(5000)
+  .end()
+  .catch(function(err) {
+    console.log(err);
+  });
